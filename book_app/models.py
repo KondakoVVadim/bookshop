dc2f4a519731c1014071d624ec2b9c103154f3b2
KondakoVVadim <komikshow8@gmail.com>
2022-12-27 02:22:45 +0200

    jopapopa

--- book_app/models.py
+++ book_app/models.py
@@ -5,3 +5,11 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=70)
     rating = models.IntegerField()
+    is_best_selling = models.BooleanField()
+    author = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return f'{self.title} - {self.rating}%'
+
+# python manage.py shell_plus --print-sql
+# from book_app.models import Book
